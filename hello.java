af0070240237cfc4a99a0dfe4c82a4b850c0d0b4
Janibek Abdurakhimov <janibekabdurakhimov357@gmail.com>
2025-05-29 12:10:16 +0500

    just added arifmetic

--- hello.java
+++ hello.java
@@ -2,5 +2,10 @@ public class hello{
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("My name is Janibek");
+        int sum;
+        int x = 5;
+        int y = 4;
+        sum = x + y;
+        System.out.println(sum);
     }
 }
